178af55d4928fcd00da06aa695f5c376c95cc622
Ashir Qureshi <ashir.qureshi@toolstop.co.uk>
2020-09-02 09:52:15 +0100

    Got basic sending of data from main to render to work

---
+++ src/indexScript.js
@@ -0,0 +1,14 @@
+// const os = require('os');
+const { ipcRenderer } = require('electron');
+
+
+
+
+const mainText = document.getElementById('main-text');
+
+ipcRenderer.on('reply-ip-addresses', (e, addresses) => {
+    mainText.textContent = addresses;
+    // console.log('addresses:', addresses);
+})
+ipcRenderer.send('get-ip-addresses', 'get-address');
+console.log("Yo");
