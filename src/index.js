5661e7dd2e3b25c18fbb759d1c0a702ebbc309b3
Ashir Qureshi <ashir.qureshi@toolstop.co.uk>
2020-09-02 13:24:59 +0100

    Added some styling to main window

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 320,
+    height: 240,
     webPreferences: {
       nodeIntegration: true
     }
